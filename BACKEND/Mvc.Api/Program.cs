25b66d97e5de6c62d4eb45b502ebb77e9485cf56
blue <i2414498@continental.edu.pe>
2026-02-28 00:23:11 -0500

    Subida de mecanica

---
+++ BACKEND/Mvc.Api/Program.cs
@@ -0,0 +1,56 @@
+using DbModel.demoDb;
+using Microsoft.EntityFrameworkCore;
+using Mvc.Bussnies.Persona;
+using Mvc.Repository.PersonaRepo.Contratos;
+using Mvc.Repository.PersonaRepo.Implementacion;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Configurar la conexión a la base de datos
+builder.Services.AddDbContext<_demoContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("demoDb");
+    // Evita AutoDetect (que abre una conexión en startup). Usa la versión conocida del servidor.
+    options.UseMySql(connectionString, ServerVersion.Parse("8.0.45-mysql"));
+});
+
+// Inyección de dependencias - Repositories
+builder.Services.AddScoped<IPersonaRepository, PersonaRepository>();
+
+// Inyección de dependencias - Business Logic
+builder.Services.AddScoped<IPersonaBussnies, PersonaBussnies>();
+
+// Configurar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
